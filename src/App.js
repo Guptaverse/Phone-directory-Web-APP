c4822b32a3694112a5fb69ec53f4d24dc780e0a6
Guptaverse <sggreat88@gmail.com>
2021-12-19 21:24:54 +0530

    Created skeleton of home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 
 function App() {
   return (
-    <div className="App">
-      phone-directory
+    <div className="App" >
+
+      <form>
+        <label htmlFor="name">Name : </label>
+        <input id="name" type="text" placeholder="Your Name" defaultValue="Shivam Gupta" />
+
+      </form>
+
+
     </div>
   );
 }
